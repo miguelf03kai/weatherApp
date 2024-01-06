5ee2fb3bc9fd96a992d2734e15f1476a2a1be10b
miguelf03kai <fernandemiguel@outlook.com>
2024-01-06 18:31:07 -0300

    Revert "Included feature no internet connection handler"
    This reverts commit c0a4028830748604d7c0e710f1823ded72843eba.

--- app/src/main/java/com/miguelf03kai/weatherapp/MainActivity.java
+++ app/src/main/java/com/miguelf03kai/weatherapp/MainActivity.java
@@ -5,9 +5,6 @@ import android.content.pm.PackageManager;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,7 +13,6 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -31,7 +27,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.miguelf03kai.weatherapp.cards.CardView;
-import com.miguelf03kai.weatherapp.utils.Connection;
 import com.miguelf03kai.weatherapp.utils.Formatter;
 import com.miguelf03kai.weatherapp.apis.WeatherAPI;
 import com.miguelf03kai.weatherapp.utils.Localization;
@@ -41,7 +36,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.sql.ResultSetMetaData;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -52,13 +46,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView conditionIconImageView, backgroundImageView;
     private TextView degreeTextView, conditionTextView, cityTextView, regionTextView, countryTextView, localTimeTextView;
     private SearchView searchView;
-    private RelativeLayout contentRelativeLayout, noConnectionRelativeLayout;
-
-    private Button btnTryAgain;
-    ConnectivityManager connectivityManager;
-    boolean connected;
-
-    Handler handler;
+    private RelativeLayout contentRelativeLayout;
 
     ArrayList<String> time, temp, speed, image;
 
@@ -91,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
 
         loaderProgressBar = (ProgressBar)findViewById(R.id.loaderProgressBar);
         contentRelativeLayout = (RelativeLayout)findViewById(R.id.contentRelativeLayout);
-        noConnectionRelativeLayout = (RelativeLayout)findViewById(R.id.noConnectionRelativeLayout);
         searchView = (SearchView)findViewById(R.id.searchView);
         degreeTextView = (TextView)findViewById(R.id.deqreeTextView);
         conditionTextView = (TextView)findViewById(R.id.conditionTextView);
@@ -101,10 +88,6 @@ public class MainActivity extends AppCompatActivity {
         conditionIconImageView = (ImageView)findViewById(R.id.conditionIconImageView);
         backgroundImageView = (ImageView)findViewById(R.id.backgroundImageView);
         localTimeTextView = (TextView)findViewById(R.id.localTimeTextView);
-
-        btnTryAgain = (Button)findViewById(R.id.button);
-
-        handler = new Handler();
 
         recyclerView = (RecyclerView)findViewById(R.id.cardsRecycleView);
         recyclerView.setHasFixedSize(true);
@@ -119,10 +102,27 @@ public class MainActivity extends AppCompatActivity {
         // Used for controlling location updates
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        // Request location permissions from device and access location
-        getLocation();
+        // Check permission for location api
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                                                    PackageManager.PERMISSION_GRANTED &&
+                                                    ActivityCompat.checkSelfPermission(this,android.Manifest.permission.
+                                                                                       ACCESS_COARSE_LOCATION) !=
+                                                    PackageManager.PERMISSION_GRANTED){
+            // Ask for permission
+            ActivityCompat.requestPermissions(MainActivity.this,
+                                                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                                                            android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                                                            PERMISSION_CODE);
+        }
 
-        checkConnection();
+        try {
+            /* This takes last location provided by network, if not have no information about location,
+             will be returned null */
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+        catch (Exception ex){
+            Log.i("PERMISSION ERROR", ex.getMessage());
+        }
 
         if(location != null)
             cityName = localization.getCityName(location.getLongitude(), location.getLatitude(),
@@ -139,7 +139,6 @@ public class MainActivity extends AppCompatActivity {
                 contentRelativeLayout.setVisibility(View.GONE);
                 loaderProgressBar.setVisibility(View.VISIBLE);
 
-                cityName = query;
                 getAndShowWeatherData(weatherAPI.requestUrl(query));
                 return false;
             }
@@ -147,16 +146,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextChange(String newText) {
                 return false;
-            }
-        });
-
-        btnTryAgain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                noConnectionRelativeLayout.setVisibility(View.GONE);
-                loaderProgressBar.setVisibility(View.VISIBLE);
-                getLocation();
-                getAndShowWeatherData(weatherAPI.requestUrl(cityName));
             }
         });
     }
@@ -175,131 +164,83 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    void getLocation(){
-        // Check permission for location api
-        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this,android.Manifest.permission.
-                        ACCESS_COARSE_LOCATION) !=
-                        PackageManager.PERMISSION_GRANTED){
-            // Ask for permission
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
-                            android.Manifest.permission.ACCESS_COARSE_LOCATION},
-                    PERMISSION_CODE);
-        }
+    void getAndShowWeatherData(String url){
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        formatter = new Formatter();
 
-        try {
-            /* This takes last location provided by network, if not have no information about location,
-             will be returned null */
-            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        }
-        catch (Exception ex){
-            Log.i("PERMISSION ERROR", ex.getMessage());
-        }
-    }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                                                                    new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // Hide progress bar.
+                loaderProgressBar.setVisibility(View.GONE);
+                // Show app content
+                contentRelativeLayout.setVisibility(View.VISIBLE);
+                try {
+                    // Get JSON objects from server response
+                    JSONObject location = response.getJSONObject("location");
+                    JSONObject current = response.getJSONObject("current");
+                    JSONObject forecastday = response.getJSONObject("forecast").getJSONArray("forecastday")
+                                                                               .getJSONObject(0);
+                    JSONArray hour = forecastday.getJSONArray("hour");
 
-    void checkConnection() {
-        // ConnectivityManager for connection checking
-        connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        // Check the internet connection
-        connected = Connection.checkConnection(connectivityManager);
-        Log.i("ACCESS NETWORK STATE", "Connected: " + connected);
-    }
+                    // Setting data to all views.
+                    cityTextView.setText(location.getString("name"));
+                    degreeTextView.setText(current.getString("temp_c")+"°c");
+                    conditionTextView.setText(current.getJSONObject("condition").getString("text"));
+                    regionTextView.setText(location.getString("region"));
+                    countryTextView.setText(location.getString("country"));
+                    localTimeTextView.setText(formatter.dateTimeFormatting(location.getString("localtime")));
 
-    void getAndShowWeatherData(final String url){
-        checkConnection();
+                    // Load the image from url using picasso library.
+                    if(current.getInt("is_day") == 0)
+                        Picasso.with(MainActivity.this).load(nightBG).into(backgroundImageView);
+                    else
+                        Picasso.with(MainActivity.this).load(dayBG).into(backgroundImageView);
 
-        handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            public void run() {
-                if(connected){
-                    RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-                    formatter = new Formatter();
+                    Picasso.with(MainActivity.this).load("https:"+current.getJSONObject("condition")
+                                                                         .getString("icon"))
+                                                                         .into(conditionIconImageView);
 
-                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                            new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    // Hide progress bar.
-                                    loaderProgressBar.setVisibility(View.GONE);
-                                    // Show app content
-                                    contentRelativeLayout.setVisibility(View.VISIBLE);
-                                    try {
-                                        // Get JSON objects from server response
-                                        JSONObject location = response.getJSONObject("location");
-                                        JSONObject current = response.getJSONObject("current");
-                                        JSONObject forecastday = response.getJSONObject("forecast").getJSONArray("forecastday")
-                                                .getJSONObject(0);
-                                        JSONArray hour = forecastday.getJSONArray("hour");
+                    time = new ArrayList<>();
+                    temp = new ArrayList<>();
+                    speed = new ArrayList<>();
+                    image = new ArrayList<>();
 
-                                        // Setting data to all views.
-                                        cityTextView.setText(location.getString("name"));
-                                        degreeTextView.setText(current.getString("temp_c")+"°c");
-                                        conditionTextView.setText(current.getJSONObject("condition").getString("text"));
-                                        regionTextView.setText(location.getString("region"));
-                                        countryTextView.setText(location.getString("country"));
-                                        localTimeTextView.setText(formatter.dateTimeFormatting(location.getString("localtime")));
+                    for(int i = 0;i < hour.length();i++){
+                        JSONObject hourObj = hour.getJSONObject(i);
+                        String timeObj = hourObj.getString("time");
+                        String temperature = hourObj.getString("temp_c");
+                        String imageObj = "https:"+hourObj.getJSONObject("condition").getString("icon");
+                        String wind = hourObj.getString("wind_kph");
 
-                                        // Load the image from url using picasso library.
-                                        if(current.getInt("is_day") == 0)
-                                            Picasso.with(MainActivity.this).load(nightBG).into(backgroundImageView);
-                                        else
-                                            Picasso.with(MainActivity.this).load(dayBG).into(backgroundImageView);
+                        time.add(timeObj);
+                        temp.add(temperature);
+                        speed.add(wind);
+                        image.add(imageObj);
+                    }
 
-                                        Picasso.with(MainActivity.this).load("https:"+current.getJSONObject("condition")
-                                                .getString("icon"))
-                                                .into(conditionIconImageView);
+                    adapterRecycleView = new CardView(time, temp, speed, image);
+                    recyclerView.setAdapter(adapterRecycleView);
 
-                                        time = new ArrayList<>();
-                                        temp = new ArrayList<>();
-                                        speed = new ArrayList<>();
-                                        image = new ArrayList<>();
+                } catch (JSONException e) {
 
-                                        for(int i = 0;i < hour.length();i++){
-                                            JSONObject hourObj = hour.getJSONObject(i);
-                                            String timeObj = hourObj.getString("time");
-                                            String temperature = hourObj.getString("temp_c");
-                                            String imageObj = "https:"+hourObj.getJSONObject("condition").getString("icon");
-                                            String wind = hourObj.getString("wind_kph");
-
-                                            time.add(timeObj);
-                                            temp.add(temperature);
-                                            speed.add(wind);
-                                            image.add(imageObj);
-                                        }
-
-                                        adapterRecycleView = new CardView(time, temp, speed, image);
-                                        recyclerView.setAdapter(adapterRecycleView);
-
-                                    } catch (JSONException e) {
-
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }, new Response.ErrorListener() {
-
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            loaderProgressBar.setVisibility(View.GONE);
-                            contentRelativeLayout.setVisibility(View.VISIBLE);
-                            Toast.makeText(MainActivity.this, "No matching location found.", Toast.LENGTH_LONG).show();
-                        }
-                    });
-
-                    queue.add(jsonObjectRequest);
-                }
-                else{
-                    loaderProgressBar.setVisibility(View.GONE);
-                    noConnectionRelativeLayout.setVisibility(View.VISIBLE);
-                    Toast.makeText(getApplicationContext(), "Unable to get weather information.", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
                 }
             }
-        }, 2000);   //2 seconds
-    }
+        }, new Response.ErrorListener() {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                loaderProgressBar.setVisibility(View.GONE);
+                contentRelativeLayout.setVisibility(View.VISIBLE);
+
+                Toast.makeText(MainActivity.this, "No matching location found.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+
     }
 }
